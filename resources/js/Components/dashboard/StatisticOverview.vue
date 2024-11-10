bc7edde1ae8c7edbebef50c105e95fc9bb78a403
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-10 08:28:31 +0800

    added initial resources for dashboard index page

---
+++ resources/js/Components/dashboard/StatisticOverview.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { DollarSign } from "lucide-vue-next";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+defineProps({
+    heading: String,
+    value: String,
+    subheading: String,
+});
+</script>
+
+<template>
+    <Card>
+        <CardHeader
+            class="flex flex-row items-center justify-between space-y-0 pb-2"
+        >
+            <CardTitle class="text-sm font-medium"> {{ heading }} </CardTitle>
+            <DollarSign class="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+            <div class="text-2xl font-bold">{{ value }}</div>
+            <p class="text-xs text-muted-foreground">{{ subheading }}</p>
+        </CardContent>
+    </Card>
+</template>
