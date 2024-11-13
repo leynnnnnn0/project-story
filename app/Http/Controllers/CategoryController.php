1a14f8542eedfa3c349a608caa0dda8cab01bdbd
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-13 19:19:51 +0800

    added categories data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Category/Index');
+        $categories = DB::select("select id,SettingName as 'CategoryCode',Value as 'CategoryName',Description,
+        case when isActive = 1 then 'Active' else 'Deactivate' end as 'Status'
+        from settings where Module = 'Category' order by id desc;");
+
+        return Inertia::render('Category/Index', [
+            'categories' => $categories
+        ]);
     }
 }
--- resources/js/Pages/Category/Index.vue
+++ resources/js/Pages/Category/Index.vue
@@ -1,5 +1,62 @@
-<script setup></script>
+<script setup>
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import Layout from "@/Layouts/App.vue";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+const props = defineProps({
+    categories: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
 
 <template>
-    <Layout heading="Categories"> </Layout>
+    <Layout heading="Categories">
+        <Card>
+            <CardHeader>
+                <CardDescription>List of categories.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Table>
+                    <TableCaption>A list of your recent orders.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead> Id </TableHead>
+                            <TableHead> Module</TableHead>
+                            <TableHead> Setting Name</TableHead>
+                            <TableHead> Description</TableHead>
+                            <TableHead> Value </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow
+                            v-for="category in categories"
+                            :key="category.id"
+                        >
+                            <TableCell>{{ category.id }}</TableCell>
+                            <TableCell>{{ category.CategoryCode }}</TableCell>
+                            <TableCell>{{ category.CategoryName }}</TableCell>
+                            <TableCell>{{ category.Description }}</TableCell>
+                            <TableCell>{{ category.Status }}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
+    </Layout>
 </template>
