0e09858d0e20035d4b32337752d68ab305e9410a
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-15 18:09:09 +0800

    updated the table on orders-approval page

--- app/Http/Controllers/OrderApprovalController.php
+++ app/Http/Controllers/OrderApprovalController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class OrderApprovalController extends Controller
 {
     public function index()
     {
-        $orders = DB::select("CALL SP_GET_LIST_SOAPPROVAL(-1)");
+        $orders = Order::with(['branch', 'vendor'])->paginate(10);
         return Inertia::render('OrderApproval/Index', [
             'orders' => $orders
         ]);
--- resources/js/Pages/OrderApproval/Index.vue
+++ resources/js/Pages/OrderApproval/Index.vue
@@ -1,65 +1,80 @@
 <script setup>
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
-
 const props = defineProps({
     orders: {
         type: Object,
     },
 });
+
+const statusBadgeColor = (status) => {
+    switch (status) {
+        case 1:
+            return "bg-green-500 text-white";
+        default:
+            return "bg-yellow-500 text-white";
+    }
+};
 </script>
 <template>
     <Layout heading="Orders For Approval List">
-        <Card>
-            <CardHeader>
-                <CardDescription
-                    >Recent transactions from your orders.</CardDescription
-                >
-            </CardHeader>
-            <CardContent>
-                <Table>
-                    <TableCaption>A list of orders for approval.</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead> Order Placed At </TableHead>
-                            <TableHead> SO Date</TableHead>
-                            <TableHead> SO Number</TableHead>
-                            <TableHead> Total Item</TableHead>
-                            <TableHead> Total Quantity </TableHead>
-                            <TableHead> Created By</TableHead>
-                            <TableHead> Vendor </TableHead>
-                            <TableHead> Branch </TableHead>
-                            <TableHead> Status </TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody v-for="order in orders">
-                        <TableCell>{{ order.OrderDate }}</TableCell>
-                        <TableCell>{{ order.SODate }}</TableCell>
-                        <TableCell>{{ order.SONumber }}</TableCell>
-                        <TableCell>{{ order.TOTAL_ITEM }}</TableCell>
-                        <TableCell>{{ order.TotalQuantity }}</TableCell>
-                        <TableCell>{{ order.CreatedBy }}</TableCell>
-                        <TableCell>{{ order.Vendor }}</TableCell>
-                        <TableCell>{{ order.Branch }}</TableCell>
-                        <TableCell>{{ order.STATUS }}</TableCell>
-                    </TableBody>
-                </Table>
-            </CardContent>
-        </Card>
+        <TableContainer>
+            <TableHeader>
+                <SearchBar />
+            </TableHeader>
+
+            <Table>
+                <TableHead>
+                    <TH> Order Placed At </TH>
+                    <TH> SO Date</TH>
+                    <TH> SO Number</TH>
+                    <TH> Total Item</TH>
+                    <TH> Total Quantity </TH>
+                    <TH> Created By</TH>
+                    <TH> Vendor </TH>
+                    <TH> Branch </TH>
+                    <TH> Status </TH>
+                </TableHead>
+                <TableBody>
+                    <tr v-for="order in orders.data">
+                        <TD>{{ order.OrderDate }}</TD>
+                        <TD>{{ order.SODate }}</TD>
+                        <TD>{{ order.SONumber }}</TD>
+                        <TD>{{ order.Total_Item }}</TD>
+                        <TD>{{ order.TOTALQUANTITY }}</TD>
+                        <TD>{{ order.CreatedBy }}</TD>
+                        <TD>{{ order.vendor?.Name ?? "N/a" }}</TD>
+                        <TD>{{ order.branch?.Name ?? "N/a" }}</TD>
+                        <TD>
+                            <Badge
+                                :class="statusBadgeColor(order.IsApproved)"
+                                class="font-bold"
+                                >{{
+                                    order.IsApproved == 1
+                                        ? "Approved"
+                                        : "Pending"
+                                }}</Badge
+                            >
+                        </TD>
+                    </tr>
+                </TableBody>
+            </Table>
+            <div
+                v-if="orders.data.length !== 0"
+                class="flex items-center justify-end gap-2"
+            >
+                <Component
+                    v-for="link in orders.links"
+                    :is="link.url ? 'Link' : 'span'"
+                    :href="link.url"
+                    v-html="link.label"
+                    class="px-3 py-1 border border-gray-200 text-primary-font font-bold rounded-lg"
+                    :class="{
+                        'bg-primary text-white': link.active,
+                        'hover:bg-primary/50 transition-colors transition-duration duration-300':
+                            link.url,
+                    }"
+                />
+            </div>
+        </TableContainer>
     </Layout>
 </template>
